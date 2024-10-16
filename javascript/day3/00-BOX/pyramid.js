4db33ac5c0facf834350fac907cd12fdfcc86f13
Théo Pruvot <33066303+theoprvo@users.noreply.github.com>
2024-10-16 16:17:30 +0200

    Update pyramid.js

--- javascript/day3/00-BOX/pyramid.js
+++ javascript/day3/00-BOX/pyramid.js
@@ -4,18 +4,30 @@ function pyramid(num) {
   }
 
   let str = "";
-  for (let i = 0; i < num; i++) {
-    for (let j = 0; j < num + (num - 1); j++) {
-      if (j % 2 === Math.round(num / 2)) {
-        str += "X";
-      } else {
-        str += "+";
-      }
+  for (let i = 1; i <= num; i++) {
+    for (let j = 0; j < num - i; j++) {
+      str += "+";
+      // a chaque incrementation de i on rajoute 2 'X'
     }
-    str += "\n";
+    // console.log(str.length);
+    for (let k = 0; k < i * 2 - 1; k++) {
+      str += "X";
+    }
+    if (i !== num) {
+      str += "\n";
+    }
   }
 
   return str;
 }
 
 console.log(pyramid(3));
+console.log(pyramid(5));
+
+// for (let j = 0; j < num + (num - 1); j++) {
+//     if (j % 2 === Math.round(num / 2)) {
+//       str += "X";
+//     } else {
+//       str += "+";
+//     }
+//   }
